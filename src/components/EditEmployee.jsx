12260e617cd02f5f87c7e7fb0d625c73ab51a4e4
danurag1906 <danurag1906@gmail.com>
2024-07-08 17:56:13 +0530

    typo in editemployee

--- src/components/EditEmployee.jsx
+++ src/components/EditEmployee.jsx
@@ -155,7 +155,7 @@ const EditEmployee = () => {
               type="text"
               id="employementtype"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter Employee ID"
+              placeholder="Enter Employement Type"
               required
               value={formData.employementtype}
               onChange={handleChange}
